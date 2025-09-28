e0c239f50a26bbbd8f681b1570d3f55639295acb
Fahmi Rahmet <fahmirahmet@gmail.com>
2025-09-28 23:31:18 +0300

    Fix function name, operations, and divide by zero handling

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,18 @@
-def perform_operation (num1,num2,operation):
-    match operation:
-        case 'add':
-            return num1 + num2 
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 == 0:
-                return f" {num1} can't be divided by 0"
-            else:
-                return num1 / num2
-        case _:
-            return "Enter a valid operation." 
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation"
+
 
 
 
