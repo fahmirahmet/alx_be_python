23fa4907d5768e2e8ec189cd627a077b14bd5f6d
Fahmi Rahmet <fahmirahmet@gmail.com>
2025-10-03 22:26:30 +0300

    Correcting some syntax errors

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,12 +1,9 @@
 def safe_divide(numerator, denominator):
     try:
-        # convert to float first
         num = float(numerator)
         den = float(denominator)
-        
         result = num / den
         return f"{num} / {den} = {result}"
-    
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
