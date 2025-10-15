b8f45287d57e7a325591e27654dd2fecd4567a4f
Fahmi Rahmet <fahmirahmet@gmail.com>
2025-10-15 18:33:14 +0300

    Removes some extra space for the damn checker

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -6,7 +6,7 @@ class Shape:
     def area(self):
         raise NotImplementedError("Subclasses must implement this method")
     
-class Rectangle (Shape):
+class Rectangle(Shape):
     def __init__(self,length, width):
         self.length = length
         self.width = width
